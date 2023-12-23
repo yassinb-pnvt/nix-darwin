8a864c966a1853f799ba4d6e8b67e8eb0ac7b565
Max Große <max12.gro@gmail.com>
2023-12-23 22:45:41 +0100

    add home

--- flake.nix
+++ flake.nix
@@ -4,65 +4,91 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
+
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs dependencies.
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
+
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-    let
-      configuration = { pkgs, ... }: {
-        # List packages installed in system profile. To search by name, run:
-        # $ nix-env -qaP | grep wget
-        environment = {
-          systemPackages =
-            [
-              pkgs.vim
-              # Better karabiner config
-              pkgs.goku
-
-            ];
-
-          # This doesn't work... idk why tho :(
-          # Hold up... maybe???
-          variables = {
-            GOKU_EDN_CONFIG_FILE = "$HOME/.config/goku/karabiner.edn";
-          };
-        };
-
-        # Auto upgrade nix package and the daemon service.
-        services = {
-          nix-daemon.enable = true;
-          karabiner-elements.enable = true;
-        };
-        # nix.package = pkgs.nix;
-
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.zsh.enable = true; # default shell on catalina
-
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 4;
-
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-      };
-    in
-    {
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#MB-Q5JMWQ5VFD
-      darwinConfigurations."MB-Q5JMWQ5VFD" = nix-darwin.lib.darwinSystem {
-        modules = [
-          configuration
-          ./modules/system.nix
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+    home-manager,
+    nixpkgs-darwin,
+  }: let
+    configuration = {pkgs, ...}: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment = {
+        systemPackages = [
+          pkgs.vim
+          # Better karabiner config
+          pkgs.goku
         ];
+
+        # This doesn't work... idk why tho :(
+        # Hold up... maybe???
+        variables = {
+          GOKU_EDN_CONFIG_FILE = "$HOME/.config/goku/karabiner.edn";
+        };
       };
 
-      # Expose the package set, including overlays, for convenience.
-      darwinPackages = self.darwinConfigurations."MB-Q5JMWQ5VFD".pkgs;
+      # Auto upgrade nix package and the daemon service.
+      services = {
+        nix-daemon.enable = true;
+        karabiner-elements.enable = true;
+      };
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true; # default shell on catalina
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
     };
+  in {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#MB-Q5JMWQ5VFD
+    darwinConfigurations."MB-Q5JMWQ5VFD" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        ./modules/system.nix
+
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.extraSpecialArgs = inputs;
+
+          # TODO replace "yourusername" with your own username!
+          home-manager.users.maxrn = import ./home;
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."MB-Q5JMWQ5VFD".pkgs;
+    # nix codee formmater
+    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.alejandra;
+  };
 }
--- modules/system.nix
+++ modules/system.nix
@@ -1,18 +1,16 @@
-{ pkgs, ... }:
-
-  ###################################################################################
-  #
-  #  macOS's System configuration
-  #
-  #  All the configuration options are documented here:
-  #    https://daiderd.com/nix-darwin/manual/index.html#sec-options
-  #  and see the source code of this project to get more undocumented options:
-  #    https://github.com/rgcr/m-cli
-  #
-  ###################################################################################
+{pkgs, ...}:
+###################################################################################
+#
+#  macOS's System configuration
+#
+#  All the configuration options are documented here:
+#    https://daiderd.com/nix-darwin/manual/index.html#sec-options
+#  and see the source code of this project to get more undocumented options:
+#    https://github.com/rgcr/m-cli
+#
+###################################################################################
 {
-
-  system = {
+  # system = {
   #   defaults = {
   #     # menuExtraClock.Show24Hour = true;  # show 24 hour clock
   #
@@ -141,7 +139,7 @@
   # };
   #
   # # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
+  # security.pam.enableSudoTouchIdAuth = true;
   #
   # # Create /etc/zshrc that loads the nix-darwin environment.
   # # this is required if you want to use darwin's default shell - zsh
@@ -178,5 +176,5 @@
   #       ];
   #     })
   #   ];
-  };
+  # };
 }
