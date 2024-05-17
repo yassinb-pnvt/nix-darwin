33a1a2ec96e5fb4463d90626e3e2f70e755a5359
Max Große <max12.gro@gmail.com>
2024-05-17 13:58:34 +0200

    fix tmux colors (maybe?)

--- home/tmux.nix
+++ home/tmux.nix
@@ -19,34 +19,35 @@
       ];
 
     extraConfig = ''
-      set -g default-shell /run/current-system/sw/bin/fish
+      set-option -ga terminal-overrides ",xterm-256color:Tc"
+      set-option -g default-terminal "tmux-256color"
 
-       # bind == bind-key
-       bind-key -r f run-shell "tmux neww tmux-sessionizer.sh"
-       bind-key c new-window -c "#{pane_current_path}"
+      # bind == bind-key
+      bind-key -r f run-shell "tmux neww tmux-sessionizer.sh"
+      bind-key c new-window -c "#{pane_current_path}"
 
-       # setw -g mode-keys vi
-       bind -T copy-mode-vi v send -X begin-selection
-       bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "pbcopy"
-       bind P paste-buffer
+      # setw -g mode-keys vi
+      bind -T copy-mode-vi v send -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "pbcopy"
+      bind P paste-buffer
 
-       # vim-like pane switching
-       bind -r ^ last-window
-       bind -r k select-pane -U
-       bind -r j select-pane -D
-       bind -r h select-pane -L
-       bind -r l select-pane -R
+      # vim-like pane switching
+      bind -r ^ last-window
+      bind -r k select-pane -U
+      bind -r j select-pane -D
+      bind -r h select-pane -L
+      bind -r l select-pane -R
 
-       # window_activity is last time of activity in current window in unix time
-       # formatting is done with strftime, see man strftime for more information
-       # set -g status-right "maxrn #{t/f/%%H#:%%M %%Y-%%-m-%%d %%A:window_activity} "
+      # window_activity is last time of activity in current window in unix time
+      # formatting is done with strftime, see man strftime for more information
+      # set -g status-right "maxrn #{t/f/%%H#:%%M %%Y-%%-m-%%d %%A:window_activity} "
 
-       # So session name doesn't get cut off
-       set -g status-left-length 20 # could be any number
+      # So session name doesn't get cut off
+      set -g status-left-length 20 # could be any number
 
-       # Session quickswitches
-       bind-key -r R run-shell "tmux-sessionizer.sh ~/uni/SGXDataCenterAttestationPrimitives"
-       bind-key -r U run-shell "tmux-sessionizer.sh ~/uni/linux-sgx"
+      # Session quickswitches
+      bind-key -r R run-shell "tmux-sessionizer.sh ~/uni/SGXDataCenterAttestationPrimitives"
+      bind-key -r U run-shell "tmux-sessionizer.sh ~/uni/linux-sgx"
     '';
   };
 }
