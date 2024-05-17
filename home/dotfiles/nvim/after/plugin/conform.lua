f720520e0f84dba09c4f36f52bf8100168376392
Max Große <max12.gro@gmail.com>
2024-05-17 08:53:49 +0200

    update formatter settings

--- home/dotfiles/nvim/after/plugin/conform.lua
+++ home/dotfiles/nvim/after/plugin/conform.lua
@@ -3,13 +3,13 @@ conform.setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		-- Conform will run multiple formatters sequentially
-		python = { "isort", "black" },
+		python = { "isort", "ruff_fix", "ruff_format" },
 		-- Use a sub-list to run only the first available formatter
 		javascript = { { "prettierd", "prettier" } },
 		typescript = { { "prettier" } },
 		typescriptreact = { { "prettier" } },
 		javascriptreact = { { "prettier" } },
-		nix = { { "alejandra" } },
+		nix = { "nixfmt" },
 	},
 	format_on_save = {
 		timeout_ms = 500,
