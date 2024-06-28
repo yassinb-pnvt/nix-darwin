fef64cdf8f8a279f6d65739b3203a2b7b15cd0ed
Max Große <max12.gro@gmail.com>
2024-06-28 09:14:14 +0200

    refactor

--- home/dotfiles/nvim/lua/plugins/conform.lua
+++ home/dotfiles/nvim/lua/plugins/conform.lua
@@ -1,24 +1,3 @@
-local opts = {
-	formatters_by_ft = {
-		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
-		python = { "isort", "ruff_fix", "ruff_format" },
-		-- Use a sub-list to run only the first available formatter
-		javascript = { { "prettierd", "prettier" } },
-		typescript = { { "prettier" } },
-		html = { { "prettierd", "prettier" } },
-		typescriptreact = { { "prettier" } },
-		javascriptreact = { { "prettier" } },
-		nix = { "nixfmt" },
-		astro = { { "prettierd", "prettier" } },
-		go = { "gofmt" },
-	},
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_fallback = false,
-	},
-}
-
 return {
 	{
 		"stevearc/conform.nvim",
@@ -32,6 +11,25 @@ return {
 				end,
 			},
 		},
-		opts = opts,
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "ruff_fix", "ruff_format" },
+				-- Use a sub-list to run only the first available formatter
+				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettier" } },
+				html = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettier" } },
+				javascriptreact = { { "prettier" } },
+				nix = { "nixfmt" },
+				astro = { { "prettierd", "prettier" } },
+				go = { "gofmt" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = false,
+			},
+		},
 	},
 }
