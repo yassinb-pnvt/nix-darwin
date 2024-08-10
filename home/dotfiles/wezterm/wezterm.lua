9d478446057e8cc3580fc81d361fa92565d19aad
Max Große <max12.gro@gmail.com>
2024-08-10 15:05:46 +0200

    remove padding

--- home/dotfiles/wezterm/wezterm.lua
+++ home/dotfiles/wezterm/wezterm.lua
@@ -63,5 +63,12 @@ config.mouse_bindings = {
 
 config.window_decorations = "NONE | RESIZE"
 
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
 -- and finally, return the configuration to wezterm
 return config
