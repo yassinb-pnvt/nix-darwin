9f34b367ec99706b8aa6d12dd12ce64442961974
Max Große <max12.gro@gmail.com>
2024-12-10 12:37:32 +0100

    upgrade lix module

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
