478fe89f1b2b4999642ff07f88998ccee9da63cc
Max Große <max12.gro@gmail.com>
2025-02-22 16:30:59 +0100

    some update (german umlauts)

--- home/dotfiles/nvim/lua/plugins/lsp.lua
+++ home/dotfiles/nvim/lua/plugins/lsp.lua
@@ -22,6 +22,8 @@ return {
                 nixd = {},
                 templ = {},
                 ts_ls = {},
+                texlab = {},
+                rust_analyzer = {},
             },
         },
         config = function(_, opts)
@@ -30,10 +32,10 @@ return {
                 config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
                 lspconfig[server].setup(config)
             end
-            vim.keymap.set('n', 'gK', function()
-                local new_config = not vim.diagnostic.config().virtual_lines
-                vim.diagnostic.config({ virtual_lines = new_config })
-            end, { desc = 'Toggle diagnostic virtual_lines' })
+            -- vim.keymap.set('n', 'gK', function()
+            --     local new_config = not vim.diagnostic.config().virtual_lines
+            --     vim.diagnostic.config({ virtual_lines = new_config })
+            -- end, { desc = 'Toggle diagnostic virtual_lines' })
         end,
     },
 }
