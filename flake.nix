b8708480d9c1542c1d305abaa6c906285910c652
Yassin Bousaadi <Yassin.Bousaadi@paynovate.com>
2025-03-14 16:58:47 +0100

    removed nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,9 +3,6 @@
 
   inputs = {
     # apparently it goes like this from fresh to stale:
-    # nixos/nixpkgs/master == nixos/nixpkgs
-    # nixos/nixpkgs/nixpkgs-unstable
-    # nixos/nixpkgs/nixos-2x.xx
     nixpkgs.url = "github:nixos/nixpkgs/master";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nix-darwin = {
@@ -14,8 +11,6 @@
     };
     home-manager = {
       url = "github:nix-community/home-manager/master";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with the `inputs.nixpkgs` of the current flake,
       # to avoid problems caused by different versions of nixpkgs dependencies.
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -25,7 +20,6 @@
     };
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
-
   outputs =
     {
       lix-module,
