87f8b4e2fa82a0da58a72f03944a870e96dd0c43
Yassin Bousaadi <yassin.bousaadi@yassins-macbook-air.curl-banana.ts.net>
2025-04-29 00:55:24 +0200

    adding fish nix

--- home/fish.nix
+++ home/fish.nix
@@ -1,4 +1,6 @@
-
+{ 
+  pkgs, ...
+ }:
 {
   home.sessionVariables = { };
 
@@ -9,11 +11,66 @@
       enableFishIntegration = true;
     };
 
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    zoxide = {
+      enable = true;
+    };
+
     fish = {
       enable = true;
       shellInit = ''
         source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
+
+        set DOCKER_CLI_EXPERIMENTAL enabled
+        set XDG_CONFIG_HOME /Users/yassin.bousaadi
+        set -gx EDITOR (which lvim)
       '';
+      plugins = [
+        { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+        { name = "fzf"; src = pkgs.fishPlugins.fzf.src; }
+        { name = "bobthefish"; src = pkgs.fishPlugins.bobthefish.src; }
+        { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
+        { name = "bass"; src = pkgs.fishPlugins.bass.src; }
+        { name = "done"; src = pkgs.fishPlugins.done.src; }
+      ];
+      shellAbbrs = {
+       # Abbreviations 
+         "gc"="git clone";
+         "gg"="lazygit";
+         "dd"="lazydocker";
+         "l"="lvim";
+         "c"="bat";
+         "py"="python3";
+
+      # Docker
+         "dc"="docker";
+         "dci"="docker image";
+         "dcct"="docker container";
+         "dcc"="docker compose";
+         "dockerprune"="docker system prune -a";
+
+      # Tf abbrevations
+         "tf"="terraform";
+         "tfa"="terraform apply";
+         "tfp"="terraform plan";
+         "tff"="terraform fmt";
+         "tfi"="terraform init";
+
+      # Ts abbrevations
+         "ts"="tailscale";
+         "tss"="tailscale status";
+         "tsw"="tailscale whois";
+      };
+      shellAliases = {
+      # Aliases
+         "l"="lvim";
+         "d"="docker";
+         "cat"="bat -p";
+         "c"="bat";
+      };
     };
   };
 }
