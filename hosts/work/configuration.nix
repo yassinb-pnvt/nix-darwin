4bed465480010633fdb81376af1635d78b3e85ed
Yassin Bousaadi <yassin.bousaadi@paynovate.com>
2025-07-23 00:05:53 +0200

    feat: added system pkgs for python and brew spotifyd

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -7,7 +7,22 @@ in
   imports = [ ../../modules/configuration.nix ];
   environment = {
     variables = {
+
     };
+    systemPackages =  [
+      pkgs.ansible
+      pkgs.awscli2
+      pkgs._1password-cli 
+      (pkgs.python3.withPackages (ps: [
+        ps.boto3
+        ps.datadog
+        ps.requests
+        ps.botocore
+        ps.boto3
+        ps.pyyaml
+      ]))
+    ];
+
   };
 
   homebrew = {
@@ -20,6 +35,7 @@ in
     casks = [ 
     "nikitabobko/tap/aerospace"
      "librewolf"
+     "spotifyd"
      ];
   };
 
@@ -32,11 +48,49 @@ in
   # Used for backwards compatibility, please read the changelog before changing.
   system = {
     stateVersion = 4;
-    defaults.smb.NetBIOSName = hostname;
+    defaults = {
+      smb.NetBIOSName = hostname;
+      dock = {
+       autohide = true;
+       orientation = "bottom";
+       showhidden = true;
+       mineffect = "genie";
+       launchanim = true;
+       show-process-indicators = true;
+       tilesize = 48;
+       static-only = true;
+       mru-spaces = false;
+       show-recents = false;
+      };
+      finder = {
+       AppleShowAllExtensions = true;
+       FXEnableExtensionChangeWarning = false;
+       CreateDesktop = false; # disable desktop icons
+     };
+    loginwindow = {
+       GuestEnabled = false;
+       DisableConsoleAccess = true;
+     };
+     SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+     NSGlobalDomain = {
+       AppleInterfaceStyle = "Dark"; # set dark mode
+       AppleKeyboardUIMode = 3;
+       ApplePressAndHoldEnabled = false;
+       NSAutomaticCapitalizationEnabled = false;
+       NSAutomaticDashSubstitutionEnabled = false;
+       NSAutomaticPeriodSubstitutionEnabled = false;
+       NSAutomaticQuoteSubstitutionEnabled = false;
+       NSAutomaticSpellingCorrectionEnabled = false;
+       NSNavPanelExpandedStateForSaveMode = true;
+       NSNavPanelExpandedStateForSaveMode2 = true;
+       _HIHideMenuBar = true;
+     };
+    };
   };
 
   nixpkgs.hostPlatform = "aarch64-darwin";
   nixpkgs.config.allowUnfree = true;
+
   networking = {
     hostName = hostname;
     computerName = hostname;
