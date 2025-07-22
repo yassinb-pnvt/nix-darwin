8bc58dc2c9a6c14fe06b4334d079c60d5f69ddee
Yassin Bousaadi <yassin.bousaadi@paynovate.com>
2025-07-23 00:05:04 +0200

    feat: added personal config

--- home/git.nix
+++ home/git.nix
@@ -1,7 +1,5 @@
 { lib, ... }:
 {
-
-  #    https://git-scm.com/docs/git-config#Documentation/git-config.txt---global
   home.activation.removeExistingGitconfig = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
     rm -f ~/.gitconfig
   '';
@@ -13,7 +11,7 @@
     userName = "Yassin Bousaadi";
     userEmail = "yassin.bousaadi@paynovate.com";
 
-    delta.enable = true; # try new differ https://github.com/dandavison/delta
+    delta.enable = true;
 
     ignores = [
       "workspace.code-workspace"
@@ -22,12 +20,15 @@
       ".direnv/"
       ".venv/"
       "__pycache__/"
+      ".cache/"
+      "*.swp"
+      "shell.nix"
     ];
 
     includes = [
       {
-        path = "~/work/.gitconfig";
-        condition = "gitdir:~/work/";
+        condition = "gitdir:~/personal/";
+        path = ".config/git/personal-config";
       }
     ];
 
@@ -35,12 +36,9 @@
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
       pull.rebase = true;
-
-      # https://jvns.ca/blog/2024/02/16/popular-git-config-options/#merge-conflictstyle-zdiff3
       merge.conflictstyle = "zdiff3";
       diff.algorithm = "histogram";
       branch.sort = "-committerdate";
-
       diff.colorMoved = "default";
       merge.tool = "nvim";
       credential.helper = "osxkeychain";
@@ -49,19 +47,22 @@
     };
 
     aliases = {
-      # common aliases
       cm = "commit -m";
       ca = "commit -am";
       cn = "commit --amend --no-edit";
       dc = "diff --cached";
-
       dft = "difftool";
       dlog = "-c diff.external=difft log -p --ext-diff";
-
-      # aliases for submodule
       update = "submodule update --init --recursive";
       foreach = "submodule foreach";
     };
   };
-}
 
+  home.file.".config/git/personal-config" = {
+    text = ''
+      [user]
+        name = Yassin Bousaâdi
+        email = yassin.bousaadi@gmail.com
+    '';
+  };
+}
